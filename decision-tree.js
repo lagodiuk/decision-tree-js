53f4ca9ef925c8608f367c3b2c45f6381f454a1f
Yurii Lahodiuk <yura_lagodiuk@ukr.net>
2013-11-23 02:16:16 +0200

    Refactoring

--- decision-tree.js
+++ decision-tree.js
@@ -1,47 +1,136 @@
-function entropy(items, categoryAttr) {
-    var counter = items.reduce(function(acc, item) { acc[item[categoryAttr]] = acc[item[categoryAttr]] ? acc[item[categoryAttr]] + 1 : 1; return acc }, {});
+function countUniqueAttributes(items, attr) {
+    //var counter = items.reduce(function(acc, item) { acc[item[categoryAttr]] = acc[item[categoryAttr]] ? acc[item[categoryAttr]] + 1 : 1; return acc }, {});
+    var counter = {};
+    
+    for(var i in items) {
+        var item = items[i];
+        
+        var attrValue = item[attr];
+        
+        if(counter[attrValue]) {
+            counter[attrValue] += 1;
+        } else {
+            counter[attrValue] = 1;
+        }
+    }
+    
+    return counter;
+}
+
+function entropy(items, attr) {
+    var counter = countUniqueAttributes(items, attr);
+    
     var entropy = 0;
-    for(var i in counter) entropy += - (counter[i] / items.length) * Math.log(counter[i] / items.length);
+    for(var i in counter) {
+        var p = counter[i] / items.length;
+        entropy += -p * Math.log(p);
+    }
+    
     return entropy;
 }
 
-var predicates = {'==' : function(a, b) {return a == b}, '>=' : function(a, b) {return a >= b}, '<=' : function(a, b) {return a <= b}};
+var predicates = {
+                    '==' : function(a, b) {return a == b},
+                    '>=' : function(a, b) {return a >= b},
+                    '<=' : function(a, b) {return a <= b}
+                 };
 
-function split(items, attribute, predicate, pivot) {
-    var result = {attribute : attribute, predicate : predicate, pivot : pivot, true : [], false : []};
-    items.forEach(function(item) {result[(item[attribute] != null) && predicates[predicate](item[attribute], pivot)].push(item)});    
+function split(items, attr, predicateName, pivot) {
+    var result = {
+                    attribute : attr,
+                    predicate : predicateName,
+                    pivot     : pivot,
+                    match     : [],
+                    notMatch  : []
+                 };
+    
+    var predicate = predicates[predicateName];
+    
+    for(var i in items) {
+        var item = items[i];
+        
+        var attrValue = item[attr];
+        
+        if((attrValue != null) && predicate(attrValue, pivot)) {
+            result.match.push(item);
+        } else {
+            result.notMatch.push(item);
+        }
+    };
+    
     return result;
 }
 
-function mostFrequentCategory(items, categoryAttr) {
-    var counter = items.reduce(function(acc, item) { acc[item[categoryAttr]] = acc[item[categoryAttr]] ? acc[item[categoryAttr]] + 1 : 1; return acc }, {});
-    var mostFrequent;
-    for(var c in counter) if(!mostFrequent || counter[c] > mostFrequent.count) mostFrequent = {category : c, count : counter[c]};
-    return mostFrequent.category;
+function mostFrequentCategory(items, attr) {
+    var counter = countUniqueAttributes(items, attr);
+    
+    var mostFrequentCount;
+    var mostFrequentCategory;
+    
+    for(var c in counter) {
+        if(mostFrequentCategory && (counter[c] < mostFrequentCount)) {
+            continue;
+        }
+        
+        mostFrequentCount = counter[c];
+        mostFrequentCategory = c;
+    };
+    return mostFrequentCategory;
 }
 
 var ENTROPY_THRESHOLD = 0.001;
+
 function buildTree(items, threshold, categoryAttr) {
-    if(!categoryAttr) categoryAttr = 'category';
-    if(!threshold) threshold = 1;
+    if(!categoryAttr) {
+        categoryAttr = 'category';
+    }
+    if(!threshold) {
+        threshold = 1;
+    }
+    
     var initialEntropy = entropy(items, categoryAttr);
-    if((initialEntropy < ENTROPY_THRESHOLD) || (items.length <= threshold)) return {category : mostFrequentCategory(items, categoryAttr)};
+    
+    if((initialEntropy < ENTROPY_THRESHOLD) || (items.length <= threshold)) {
+        return {category : mostFrequentCategory(items, categoryAttr)}
+    };
+    
     var bestSplit; 
-    items.forEach(function(item) {
-        for(var attr in item) if(attr !== categoryAttr) {            
+    for(var i in items) {
+        var item = items[i];
+        
+        for(var attr in item) {
+            if(attr == categoryAttr) {
+                continue;
+            }
+            
             for(var predicate in predicates) {
-                var currSplit = split(items, attr, predicate, item[attr]);            
-                currSplit.gain = initialEntropy - (entropy(currSplit.true, categoryAttr) * currSplit.true.length + entropy(currSplit.false, categoryAttr) * currSplit.false.length) / items.length;
+                
+                var currSplit = split(items, attr, predicate, item[attr]);
+                
+                currSplit.gain = initialEntropy - (entropy(currSplit.match, categoryAttr) * currSplit.match.length + entropy(currSplit.notMatch, categoryAttr) * currSplit.notMatch.length) / items.length;
+                
                 if(!bestSplit || (currSplit.gain > 0 && bestSplit.gain < currSplit.gain)) bestSplit = currSplit;
             }
         }
-    });   
-    return {attribute : bestSplit.attribute, predicate : bestSplit.predicate, pivot : bestSplit.pivot, true : buildTree(bestSplit.true, threshold, categoryAttr), false : buildTree(bestSplit.false, threshold, categoryAttr)};
+    }
+    return {attribute : bestSplit.attribute, predicate : bestSplit.predicate, pivot : bestSplit.pivot, match : buildTree(bestSplit.match, threshold, categoryAttr), notMatch : buildTree(bestSplit.notMatch, threshold, categoryAttr)};
 }
 
 function predict(tree, item) {
-    if(tree.category) return tree.category;
-    return predict(tree[(item[tree.attribute] != null) && predicates[tree.predicate](item[tree.attribute], tree.pivot)], item);
+    if(tree.category) {
+        return tree.category;
+    }
+    
+    var attrName = tree.attribute;
+    var attrValue = item[attrName];
+    var predicateName = tree.predicate;
+    var predicate = predicates[predicateName];
+    var pivot =  tree.pivot;
+    if((attrValue != null) && predicate(attrValue, pivot)) {
+        return predict(tree.match, item);
+    } else {
+        return predict(tree.notMatch, item);
+    }
 }
 
 
