f6939e95e9c4a01eb7b745902f039dfe1c318644
Yurii Lahodiuk <yura_lagodiuk@ukr.net>
2013-11-29 02:29:24 +0200

    Encapsulation: constructors DecisionTree and RandomForest

--- decision-tree.js
+++ decision-tree.js
@@ -1,231 +1,234 @@
-function countUniqueAttributes(items, attr) {
-    var counter = {};
+function DecisionTree(builder) {
     
-    for(var i in items) {
-        var item = items[i];
-        
-        var attrValue = item[attr];
-        
-        if(counter[attrValue]) {
-            counter[attrValue] += 1;
-        } else {
-            counter[attrValue] = 1;
-        }
-    }
-    
-    return counter;
-}
-
-function entropy(items, attr) {
-    var counter = countUniqueAttributes(items, attr);
-    
-    var entropy = 0;
-    for(var i in counter) {
-        var p = counter[i] / items.length;
-        entropy += -p * Math.log(p);
-    }
-    
-    return entropy;
-}
-
-var predicates = {
-                    '==' : function(a, b) {return a == b},
-                    '>=' : function(a, b) {return a >= b},
-                    '<=' : function(a, b) {return a <= b}
-                 };
-
-function split(items, attr, predicateName, pivot) {
-    var result = {
-                    attribute : attr,
-                    predicate : predicateName,
-                    pivot     : pivot,
-                    match     : [],
-                    notMatch  : []
-                 };
-    
-    var predicate = predicates[predicateName];
-    
-    for(var i in items) {
-        var item = items[i];
-        
-        var attrValue = item[attr];
-        
-        if((attrValue != null) && predicate(attrValue, pivot)) {
-            result.match.push(item);
-        } else {
-            result.notMatch.push(item);
-        }
+    var predicates = {
+        '==' : function(a, b) {return a == b},
+        '>=' : function(a, b) {return a >= b},
+        '<=' : function(a, b) {return a <= b}
     };
     
-    return result;
-}
-
-function mostFrequentCategory(items, attr) {
-    var counter = countUniqueAttributes(items, attr);
-    
-    var mostFrequentCount;
-    var mostFrequentCategory;
-    
-    for(var c in counter) {
-        if(!mostFrequentCategory || (counter[c] > mostFrequentCount)) {
+    var countUniqueAttributes = function(items, attr) {
+        var counter = {};
+        
+        for(var i in items) {
+            var item = items[i];
             
-            mostFrequentCount = counter[c];
-            mostFrequentCategory = c;
-        }
-    };
-    
-    return mostFrequentCategory;
-}
-
-function buildDecisionTree(builder) {
-    
-    var items = builder.trainingSet;
-    var threshold = builder.minItemsCount;
-    var categoryAttr = builder.categoryAttr;
-    var entropyThrehold = builder.entropyThrehold;
-    var maxTreeDepth = builder.maxTreeDepth;
-
-    // Default values
-    if(!categoryAttr) {
-        categoryAttr = 'category';
-    }
-    if(!threshold) {
-        threshold = 1;
-    }
-    if(!entropyThrehold) {
-        entropyThrehold = 0.01;
-    }
-    if(!maxTreeDepth) {
-        maxTreeDepth = 70;
-    }
-    
-    var initialEntropy = entropy(items, categoryAttr);
-    
-    if((maxTreeDepth == 0) || (initialEntropy < entropyThrehold) || (items.length <= threshold)) {
-        return {category : mostFrequentCategory(items, categoryAttr)}
-    };
-    
-    var bestSplit = {
-        gain : 0
-    };
-    
-    for(var i in items) {
-        var item = items[i];
-        
-        for(var attr in item) {
-            if(attr == categoryAttr) {
-                continue;
+            var attrValue = item[attr];
+            
+            if(counter[attrValue]) {
+                counter[attrValue] += 1;
+            } else {
+                counter[attrValue] = 1;
             }
+        }
+        
+        return counter;
+    }
+    
+    var entropy = function(items, attr) {
+        var counter = countUniqueAttributes(items, attr);
+        
+        var entropy = 0;
+        for(var i in counter) {
+            var p = counter[i] / items.length;
+            entropy += -p * Math.log(p);
+        }
+        
+        return entropy;
+    }
+    
+    var split = function(items, attr, predicateName, pivot) {
+        var result = {
+            attribute : attr,
+            predicate : predicateName,
+            pivot     : pivot,
+            match     : [],
+            notMatch  : []
+        };
+        
+        var predicate = predicates[predicateName];
+        
+        for(var i in items) {
+            var item = items[i];
             
-            for(var predicate in predicates) {
+            var attrValue = item[attr];
+            
+            if((attrValue != null) && predicate(attrValue, pivot)) {
+                result.match.push(item);
+            } else {
+                result.notMatch.push(item);
+            }
+        };
+        
+        return result;
+    }
+    
+    var mostFrequentCategory = function(items, attr) {
+        var counter = countUniqueAttributes(items, attr);
+        
+        var mostFrequentCount;
+        var mostFrequentCategory;
+        
+        for(var c in counter) {
+            if(!mostFrequentCategory || (counter[c] > mostFrequentCount)) {
                 
-                var currSplit = split(items, attr, predicate, item[attr]);
+                mostFrequentCount = counter[c];
+                mostFrequentCategory = c;
+            }
+        };
+        
+        return mostFrequentCategory;
+    }
+    
+    var buildDecisionTree = function(builder) {
+        
+        var trainingSet = builder.trainingSet;
+        var minItemsCount = builder.minItemsCount;
+        var categoryAttr = builder.categoryAttr;
+        var entropyThrehold = builder.entropyThrehold;
+        var maxTreeDepth = builder.maxTreeDepth;
+        
+        var initialEntropy = entropy(trainingSet, categoryAttr);
+        
+        if((maxTreeDepth == 0) || (initialEntropy < entropyThrehold) || (trainingSet.length <= minItemsCount)) {
+            return {category : mostFrequentCategory(trainingSet, categoryAttr)}
+        };
+        
+        var bestSplit = {
+            gain : 0
+        };
+        
+        for(var i in trainingSet) {
+            var item = trainingSet[i];
+            
+            for(var attr in item) {
+                if(attr == categoryAttr) {
+                    continue;
+                }
                 
-                var matchEntropy = entropy(currSplit.match, categoryAttr);
-                var notMatchEntropy = entropy(currSplit.notMatch, categoryAttr);
-                
-                var newEntropy = 0;
-                newEntropy += matchEntropy * currSplit.match.length;
-                newEntropy += notMatchEntropy * currSplit.notMatch.length;
-                newEntropy /= items.length;
-                
-                currSplit.gain = initialEntropy - newEntropy;
-                
-                if(currSplit.gain > 0 && bestSplit.gain < currSplit.gain) {
-                    bestSplit = currSplit;
+                for(var predicate in predicates) {
+                    
+                    var currSplit = split(trainingSet, attr, predicate, item[attr]);
+                    
+                    var matchEntropy = entropy(currSplit.match, categoryAttr);
+                    var notMatchEntropy = entropy(currSplit.notMatch, categoryAttr);
+                    
+                    var newEntropy = 0;
+                    newEntropy += matchEntropy * currSplit.match.length;
+                    newEntropy += notMatchEntropy * currSplit.notMatch.length;
+                    newEntropy /= trainingSet.length;
+                    
+                    currSplit.gain = initialEntropy - newEntropy;
+                    
+                    if(currSplit.gain > 0 && bestSplit.gain < currSplit.gain) {
+                        bestSplit = currSplit;
+                    }
                 }
             }
         }
-    }
-    
-    if(bestSplit.gain <= 0) {
-        // Can't find optimal split
-        return {category : mostFrequentCategory(items, categoryAttr)}
-    }
-    
-    return {attribute : bestSplit.attribute,
+        
+        if(bestSplit.gain <= 0) {
+            // Can't find optimal split
+            return {category : mostFrequentCategory(trainingSet, categoryAttr)}
+        }
+        
+        return {attribute : bestSplit.attribute,
             predicate : bestSplit.predicate,
             pivot     : bestSplit.pivot,
             match     : buildDecisionTree({
-                                            trainingSet : bestSplit.match,
-                                            minItemsCount : threshold,
-                                            categoryAttr : categoryAttr,
-                                            entropyThrehold : entropyThrehold,
-                                            maxTreeDepth : maxTreeDepth - 1
+                                          trainingSet : bestSplit.match,
+                                          minItemsCount : minItemsCount,
+                                          categoryAttr : categoryAttr,
+                                          entropyThrehold : entropyThrehold,
+                                          maxTreeDepth : maxTreeDepth - 1
                                           }),
             notMatch  : buildDecisionTree({
-                                            trainingSet : bestSplit.notMatch,
-                                            minItemsCount : threshold,
-                                            categoryAttr : categoryAttr,
-                                            entropyThrehold : entropyThrehold,
-                                            maxTreeDepth : maxTreeDepth - 1
+                                          trainingSet : bestSplit.notMatch,
+                                          minItemsCount : minItemsCount,
+                                          categoryAttr : categoryAttr,
+                                          entropyThrehold : entropyThrehold,
+                                          maxTreeDepth : maxTreeDepth - 1
                                           })};
-}
-
-function predict(tree, item) {
-    if(tree.category) {
-        return tree.category;
     }
     
-    var attrName = tree.attribute;
-    var attrValue = item[attrName];
+    var predict = function(tree, item) {
+        if(tree.category) {
+            return tree.category;
+        }
+        
+        var attrName = tree.attribute;
+        var attrValue = item[attrName];
+        
+        var predicateName = tree.predicate;
+        var predicate = predicates[predicateName];
+        
+        var pivot =  tree.pivot;
+        
+        if((attrValue != null) && predicate(attrValue, pivot)) {
+            return predict(tree.match, item);
+        } else {
+            return predict(tree.notMatch, item);
+        }
+    }
     
-    var predicateName = tree.predicate;
-    var predicate = predicates[predicateName];
+    if(builder.removeDefaultPredicates) {
+        for(var p in builder.removeDefaultPredicates) {
+            delete predicates[p];
+        }
+    }
     
-    var pivot =  tree.pivot;
+    this.root = buildDecisionTree({
+                                  trainingSet : builder.trainingSet,
+                                  categoryAttr : (builder.categoryAttr ? builder.categoryAttr : 'category'),
+                                  minItemsCount : (builder.minItemsCount ? builder.minItemsCount : 1),
+                                  entropyThrehold : (builder.entropyThrehold ? builder.entropyThrehold : 0.01),
+                                  maxTreeDepth : (builder.maxTreeDepth ? builder.maxTreeDepth : 70)
+                                  });
     
-    if((attrValue != null) && predicate(attrValue, pivot)) {
-        return predict(tree.match, item);
-    } else {
-        return predict(tree.notMatch, item);
+    this.predict = function(item) {
+        return predict(this.root, item);
     }
 }
 
-function buildRandomForest(builder) {
-    var items = builder.trainingSet;
-    var categoryAttr = builder.categoryAttr;
-    var treesNumber = builder.treesNumber;
-    var maxTreeDepth = builder.maxTreeDepth;
+function RandomForest(builder, treesNumber) {
     
-    var forest = [];
+    var buildRandomForest = function(builder, treesNumber) {
+        var items = builder.trainingSet;
+        
+        var forest = [];
+        
+        for(var t = 0; t < treesNumber; t++) {
+            
+            builder.trainingSet = [];
+            
+            for(var i = 1; i <= items.length; i++) {
+                if(i % (t + 2) == 0) {
+                    builder.trainingSet.push(items[i - 1]);
+                }
+            }
+            
+            var tree = new DecisionTree(builder);
+            forest.push(tree);
+        }
+        return forest;
+    }
     
-    for(var t = 0; t < treesNumber; t++) {
-        
-        var treeBuilder = {
-            trainingSet : []
-        };
-        
-        for(var i = 1; i <= items.length; i++) {
-            if(i % (t + 2) == 0) {
-                treeBuilder.trainingSet.push(items[i - 1]);
+    var predictRandomForest = function(forest, item) {
+        var result = {};
+        for(var i in forest) {
+            var tree = forest[i];
+            var prediction = tree.predict(item);
+            if(result[prediction]) {
+                result[prediction] += 1;
+            } else {
+                result[prediction] = 1;
             }
         }
-        
-        if(categoryAttr) {
-            treeBuilder.categoryAttr = categoryAttr;
-        }
-        
-        if(maxTreeDepth) {
-            treeBuilder.maxTreeDepth = maxTreeDepth;
-        }
-        
-        var tree = buildDecisionTree(treeBuilder);
-        forest.push(tree);
+        return result;
     }
-    return forest;
-}
-
-function predictRandomForest(forest, item) {
-    var result = {};
-    for(var i in forest) {
-        var tree = forest[i];
-        var prediction = predict(tree, item);
-        if(result[prediction]) {
-            result[prediction] += 1;
-        } else {
-            result[prediction] = 1;
-        }
+    
+    this.trees = buildRandomForest(builder, treesNumber);
+    
+    this.predict = function(item) {
+        return predictRandomForest(this.trees, item);
     }
-    return result;
 }
